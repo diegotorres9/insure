ae23e428ed2033326b6fde3c7da8a68c4012978a
diegotorres9 <diegoetorres1@gmail.com>
2020-09-17 21:56:39 -0400

    new until class to prevent scrolling on mobile menu open

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 var menuIcon = document.querySelector('.mobile-icon');
 var mobileMenu = document.querySelector('.mobile-menu');
+var body= document.querySelector('body');
 menuIcon.addEventListener('click', function() {
     // console.log('icon clicked');
     mobileMenu.classList.toggle('hide');
+    body.classList.toggle('noscroll');
 });
 // console.log(menuIcon);
