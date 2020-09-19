0b19acd2ba097ccd5253a203c8fa57135cff48ec
diegotorres9 <diegoetorres1@gmail.com>
2020-09-18 21:40:52 -0400

    further bg-pattern styling and javascript close toggle

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 var menuIcon = document.querySelector('.mobile-icon');
 var mobileMenu = document.querySelector('.mobile-menu');
-var body= document.querySelector('body');
+var body = document.querySelector('body');
 menuIcon.addEventListener('click', function() {
     // console.log('icon clicked');
     mobileMenu.classList.toggle('hide');
     body.classList.toggle('noscroll');
+    if(mobileMenu.classList.contains('hide')){
+        menuIcon.src = "./images/icon-hamburger.svg";
+    } else {
+        menuIcon.src = "./images/icon-close.svg";
+    }
+    
 });
 // console.log(menuIcon);
